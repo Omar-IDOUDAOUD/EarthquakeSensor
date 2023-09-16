ce6424162bb0c4f52f45253b993aee29a0de2016
Omar-IDOUDAOUD <pcomar.lenovo@gmail.com>
2023-09-17 00:23:01 +0100

    add 5th testg

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,19 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
+class LogItem {
+  final double x;
+  final double y;
+  final DateTime dateTime;
+  final Duration duration;
+
+  LogItem(
+      {required this.x,
+      required this.y,
+      required this.dateTime,
+      required this.duration});
+}
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -13,6 +26,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List<LogItem> _eventsLog = List.generate(50, (index) {
+    return LogItem(
+        x: 10,
+        y: index.toDouble(),
+        dateTime: DateTime.now(),
+        duration: Duration(milliseconds: 500));
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,15 +50,45 @@ class _HomeState extends State<Home> {
           ),
         ),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.history_rounded,
+              color: Colors.blueGrey.shade900,
+            ),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (_) {
+                    return ListView.separated(
+                      reverse: true,
+                      padding: const EdgeInsets.all(15),
+                      itemCount: _eventsLog.length,
+                      separatorBuilder: (context, index) => SizedBox(
+                        height: 5,
+                      ),
+                      itemBuilder: (_, i) {
+                        return Text(_eventsLog.elementAt(i).y.toString());
+                      },
+                    );
+                  });
+            },
+          ),
+        ],
       ),
       backgroundColor: Colors.white,
-      body: _Body(),
+      body: _Body(
+        onAddLog: (log) {
+          _eventsLog.add(log);
+        },
+      ),
     );
   }
 }
 
 class _Body extends StatefulWidget {
-  const _Body({super.key});
+  const _Body({super.key, required this.onAddLog});
+  final Function(LogItem) onAddLog;
 
   @override
   State<_Body> createState() => _BodyState();
@@ -53,7 +104,8 @@ class _BodyState extends State<_Body> {
   late final Size _c2s;
   late final Size _c3s;
   Offset _indexCircleOffset = Offset.zero;
-
+  bool _useDangerColor = false;
+  final _accelerometerStream = SensorsPlatform.instance.accelerometerEvents;
   @override
   void initState() {
     // TODO: implement initState
@@ -64,16 +116,18 @@ class _BodyState extends State<_Body> {
     //   //1 => 1*(_c1s-_cls3)/50
     // });
 
-    SensorsPlatform.instance.accelerometerEvents.listen((event) {
+    _accelerometerStream.listen((event) {
       final sfactor = Size.square(_c1s.width - _c3s.width);
       final dx = event.x * sfactor.width / _maxRecordValue;
       final dy = event.y * sfactor.width / _maxRecordValue;
-      final dz = event.z * sfactor.width / _maxRecordValue;
+
       setState(() {
-        if (dx >= _minAlertValue ||
-            dz >= _minAlertValue ||
-            dy >= _minAlertValue) _eventsLog.add("x = $dx, y = $dy, z = $dz");
-        _indexCircleOffset = Offset(dx + (dx.isNegative ? dz : dz.ceil()), dy);
+        if (dx >= _minAlertValue || dy >= _minAlertValue) {
+          // widget.onAddLog("x = $dx, y = $dy");
+          // _startRecordingLog();
+          _useDangerColor = true;
+        }
+        _indexCircleOffset = Offset(dx, dy);
       });
     });
   }
@@ -85,6 +139,19 @@ class _BodyState extends State<_Body> {
     _c2s = _c1s / 1.5;
     _c3s = _c1s / 3;
   }
+  // bool _itsRecordingLogCurrently = false;
+  // void _startRecordingLog(){
+  //   if (_itsRecordingLogCurrently) return;
+  //   _itsRecordingLogCurrently = true;
+  //   final List<double> xColl = [];
+  //   final List<double> yColl = [];
+  //   _accelerometerStream.listen((event) {
+  //     xColl.add(event.x);
+  //     yColl.add(event.y);
+  //   });
+
+  //   // Duration(milliseconds: 500)
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -96,7 +163,7 @@ class _BodyState extends State<_Body> {
           child: Row(
             children: [
               Text(
-                'min alert value: $_minAlertValue',
+                'min alert value: ${_minAlertValue.toStringAsFixed(1)}',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 15,
@@ -106,10 +173,12 @@ class _BodyState extends State<_Body> {
               IconButton(
                 icon: Icon(Icons.remove),
                 onPressed: () {
-                  if (_minAlertValue == 0) return;
-
+                  if (_minAlertValue <= 0.1) return;
                   setState(() {
-                    _minAlertValue -= 0.5;
+                    if (_minAlertValue <= 1)
+                      _minAlertValue -= 0.1;
+                    else
+                      _minAlertValue -= 0.5;
                   });
                 },
               ),
@@ -117,7 +186,10 @@ class _BodyState extends State<_Body> {
                 icon: Icon(Icons.add),
                 onPressed: () {
                   setState(() {
-                    _minAlertValue += 0.5;
+                    if (_minAlertValue < 1)
+                      _minAlertValue += 0.1;
+                    else
+                      _minAlertValue += 0.5;
                   });
                 },
               ),
@@ -129,7 +201,7 @@ class _BodyState extends State<_Body> {
           child: Row(
             children: [
               Text(
-                'max record value: $_maxRecordValue',
+                'max record value: ${_maxRecordValue}',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 15,
@@ -157,11 +229,9 @@ class _BodyState extends State<_Body> {
           ),
         ),
         SizedBox(height: 10),
-        SizedBox.square(
-          dimension: _c1s.height,
+        Expanded(
           child: Stack(
             alignment: Alignment.center,
-            // fit: StackFit.expand,
             children: [
               Container(
                 height: _c1s.height,
@@ -183,6 +253,7 @@ class _BodyState extends State<_Body> {
                 onEnd: () {
                   setState(() {
                     _indexCircleOffset = Offset.zero;
+                    _useDangerColor = false;
                   });
                 },
                 duration: Duration(milliseconds: 200),
@@ -190,33 +261,23 @@ class _BodyState extends State<_Body> {
                   ..setTranslationRaw(
                     _indexCircleOffset.dx,
                     _indexCircleOffset.dy,
-                    _indexCircleOffset.dx,
+                    0,
                   ),
                 height: _c3s.height,
                 width: _c3s.width,
                 decoration: BoxDecoration(
-                  color: Colors.greenAccent,
+                  color:
+                      _useDangerColor ? Colors.redAccent : Colors.greenAccent,
                   shape: BoxShape.circle,
                 ),
               ),
             ],
           ),
         ),
-        Expanded(
-          child: ListView.separated(
-            padding: const EdgeInsets.all(15),
-            itemCount: _eventsLog.length,
-            separatorBuilder: (context, index) => SizedBox(
-              height: 5,
-            ),
-            itemBuilder: (_, i) {
-              return Text(_eventsLog.elementAt(i));
-            },
-          ),
-        )
+        // Expanded(
+        //
+        // )
       ],
     );
   }
-
-  final List<String> _eventsLog = List.empty(growable: true);
 }
